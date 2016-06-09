18610aa894037329eced9181b779806c3652c0cb
Amine <amine@amine.amine>
2016-06-09 06:24:03 +0100

    Remove hardcoding in run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app.main import start
 from sys import argv
 
-start('127.6.68.129', 8080, '/var/lib/openshift/5758b6360c1e66560600000a/app-root/runtime/repo/')
+start(argv[1], argv[2], argv[3])
