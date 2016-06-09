898da83d3476db08425094355de8ff899ffd4507
Amine <amine@amine.amine>
2016-06-09 23:56:45 +0100

    Try fixing doc show #14

--- app/ftp.py
+++ app/ftp.py
@@ -4,5 +4,8 @@ ftp_app = Bottle()
 	
 @ftp_app.route('/ftp/<filepath:path>', method='GET')
 def site_ftp(filepath):
+	return filepath
+	'''
     return static_file(filepath, root=ftp_app.config.get('root_dir')+'/ftp', 
     						download=True)
+    '''
