46cf8fffd5dcdfc4a0d8b8342ba57affef2374f6
Amine <amine@amine.amine>
2016-06-10 00:32:36 +0100

    Try fixing doc show #26

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from bottle import Bottle, run, static_file, template
 from .web import web_app
 from .ftp import ftp_app
+from .mongodb import mongodb_app
 
 site_app = Bottle()
 
 site_app.merge(ftp_app)
 site_app.merge(web_app)
+site_app.merge(mongodb_app)
 
 def start(h='localhost', p=80, d='.', db_h='localhost', db_p=3306):
 	web_app.config['root_dir'] = d
 	ftp_app.config['root_dir'] = d
+	mongodb_app.config['mongodb_db_host'] = db_h
+	mongodb_app.config['mongodb_db_port'] = db_p
 	run(site_app, host=h, port=p, reloader=True)
--- app/mongodb.py
+++ app/mongodb.py
@@ -19,7 +19,7 @@ def post_document():
     except Exception as ve:
         abort(400, str(ve))
 
-@mongodb_app.route('/documents/<id>', method='POST')
+@mongodb_app.route('/documents/<id>', method='GET')
 def get_document(id):
     entity = db.docs.find_one({'_id':id})
     if not entity:
