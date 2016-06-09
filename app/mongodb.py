9df8130aa43c57ef87f41bbaef991e18cdd38adb
Amine <amine@amine.amine>
2016-06-10 00:14:57 +0100

    Try fixing doc show #23

--- app/mongodb.py
+++ app/mongodb.py
@@ -1,5 +1,5 @@
 import json
-from bottle import Bottle, request, abort, get
+from bottle import Bottle, request, abort
 from pymongo import MongoClient
 
 mongodb_app = Bottle()
@@ -7,18 +7,8 @@ mongodb_app = Bottle()
 client = MongoClient('mongodb://admin:veAcrpgiBLl5@127.6.68.130:27017')
 db = client.diy
 
-'''
-@mongodb_app.route('/documents/<id>', method='GET')
-def get_document(id):
-    entity = db.docs.find_one({'_id':id})
-    if not entity:
-        abort(404, 'No document found')
-    return entity
-'''
-
-@mongodb_app.get('/documents/<a>')
-def post_document(a):
-    '''
+@mongodb_app.route('/documents', method='POST')
+def post_document():
     entity = request.json
     if not entity:
         abort(400, 'No data received')
@@ -28,6 +18,10 @@ def post_document(a):
         db.docs.insert_one(entity)
     except Exception as ve:
         abort(400, str(ve))
-    '''
 
+@mongodb_app.route('/documents/<id>', method='GET')
+def get_document(id):
+    entity = db.docs.find_one({'_id':id})
+    if not entity:
+        abort(404, 'No document found')
     return entity
