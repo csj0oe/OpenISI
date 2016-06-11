5b4f9154a20985c943717cad896aa4c60c16a80a
Amine <amine@amine.amine>
2016-06-11 19:14:31 +0100

    Convert port type to integer

--- wsgi.py
+++ wsgi.py
@@ -8,17 +8,15 @@ from app.main import site_app as application
 
 # Define server host
 host = argv[1] if len(argv) > 1 else 'localhost'
-print(type(host))
-print(host)
 
 # Define server port
-port = argv[2] if len(argv) > 2 else 8080
+port = int(argv[2]) if len(argv) > 2 else 8080
 
 # Define mongodb host
 application.config['mongodb_host'] = argv[3] if len(argv) > 3 else 'localhost'
 
 # Define mongodb port
-application.config['mongodb_port'] = argv[4] if len(argv) > 4 else 27017
+application.config['mongodb_port'] = int(argv[4]) if len(argv) > 4 else 27017
 
 if __name__ == '__main__':
     httpd = make_server(host, port, application)
