9ef95614d817a280bd767e50832cba7de22da5b2
Amine <amine@amine.amine>
2016-06-11 17:00:01 +0100

    Add config to ftp module

--- app/ftp.py
+++ app/ftp.py
@@ -2,9 +2,10 @@ import os
 
 from bottle import Bottle, static_file
 
+from .config import repo_dir
+
 ftp_app = Bottle()
 	
 @ftp_app.route('/ftp/<filepath:path>', method='GET')
 def site_ftp(filepath):
-    return static_file(filepath, root=ftp_app.config.get('root_dir')+'/ftp', 
-    						download=True)
+    return static_file(filepath, root=repo_dir+'/ftp', download=True)
