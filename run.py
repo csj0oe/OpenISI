7953c8856c5220f6e6a3a1437b09ceb6500b9c48
Amine <amine@amine.amine>
2016-06-11 16:35:55 +0100

    Try new run file

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
-from app.main import start
+#!/usr/bin/env python
+
 from sys import argv
 
-start(argv[1], argv[2], argv[3], argv[4], argv[5])
+from wsgiref.simple_server import make_server
+
+from app.main import site_app as application
+
+# Define server host
+host = argv[1] if len(argv) > 1 else 'localhost'
+
+# Define server port
+port = argv[2] if len(argv) > 2 else 8080
+
+# Define mongodb host
+application.config['mongodb_host'] = argv[3] if len(argv) > 3 else 'localhost'
+
+# Define mongodb port
+application.config['mongodb_port'] = argv[4] if len(argv) > 4 else 27017
+
+if __name__ == '__main__':
+    httpd = make_server(host, port, application)
+    # Wait for a single request, serve it and quit.
+    #httpd.handle_request()
+    httpd.serve_forever()
