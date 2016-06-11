1272db2fb6c7ebcb19a97e361827ba831e06fb61
Amine <amine@amine.amine>
2016-06-11 21:11:15 +0100

    Redirect all requests to web

--- app/web.py
+++ app/web.py
@@ -1,14 +1,17 @@
 import os
 
-from bottle import Bottle, static_file
+from bottle import Bottle, static_file, redirect
 
 from .config import repo_dir
 
 web_app = Bottle()
 	
 @web_app.route('/', method='GET')
+def redirect_web():
+	redirect('/web')
+
 @web_app.route('/web', method='GET')
 @web_app.route('/web/', method='GET')
 @web_app.route('/web/<filepath:path>', method='GET')
-def site_web(filepath='index.html'):
+def site_web(filepath='site.html'):
     return static_file(filepath, root=repo_dir+'/web')
