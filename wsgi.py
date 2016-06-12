002d149fbf4e0bfc9ac0bc3e2c5b4cbed28da03f
Amine <amine@amine.amine>
2016-06-12 14:41:55 +0100

    Move import statement to top of file

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,9 @@
 from wsgiref.simple_server import make_server
 
 from app.config import *
+from app.main import site_app as application
 
 if __name__ == '__main__':
-    from app.main import site_app as application
     httpd = make_server(host, port, application)
     # Wait for a single request, serve it and quit.
     #httpd.handle_request()
